dd4a270bacff08684f5ba740ac8dc1cf6dedf5a8
destoer <destoer1234@protonmail.com>
2023-08-23 23:21:41 +0100

    add basic file bin test

--- src/test.cpp
+++ src/test.cpp
@@ -197,6 +197,7 @@ static constexpr ProgramCorrectTest PROGRAM_CORRECT_TEST[] =
     {"tests/stl/mem",0},
     {"tests/stl/alloc",1},
     {"tests/stl/file_str",6},
+    {"tests/stl/file_bin",57},
 
 
 
