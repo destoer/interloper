c5bac01f30cf358c5c86ac08d51d71f3987b7ffe
destoer <destoer1234@protonmail.com>
2023-11-27 14:17:28 +0000

    start defining text section

--- src/elf.cpp
+++ src/elf.cpp
@@ -2,16 +2,34 @@
 
 #include <elf.h>
 
+struct TextSection
+{
+    // program storage
+    Array<u8> buffer;
+
+    // what offset is this stored at in the text section?
+    // NOTE: this is relative to the start of the buffer
+    // and not the elf file as a whole
+    HashTable<String,u32> offset_table;
+};
+
 // NOTE: atm we only care about supporting 64 bit
 struct Elf
 {
+    // output file
     String name;
-    
+
+    // final buffer
+    Array<u8> buffer;
+
+    // TODO: these shouldn't be hard coded
     arch_target arch = arch_target::x86_64_t;
     os_target os = os_target::linux_t;
 
     // basic elf header
     Elf64_Ehdr header;
+
+    TextSection text_section;
 };
 
 
@@ -87,17 +105,47 @@ Elf make_elf(const String& filename)
     // setup initial header section
     setup_header(elf);
 
+    elf.text_section.offset_table = make_table<String,u32>();
 
     return elf;
 }
 
+void finalise_elf(Elf& elf)
+{
+    // add header
+    push_mem(elf.buffer,&elf.header,sizeof(elf.header));
+
+    // add text section
+    push_mem(elf.buffer,elf.text_section.buffer);
+    
+    // text section added, setup entry point
+
+    // add symtab entires for functions now we know where the text section lives
+
+
+    printf("program size: %x\n",elf.buffer.size);
+}
+
 void destroy_elf(Elf& elf)
 {
-    UNUSED(elf);
+    // text section
+    destroy_arr(elf.text_section.buffer);
+    destroy_table(elf.text_section.offset_table);
+
+    destroy_arr(elf.buffer);
 }
 
 // insert a new function into the elf file
-void add_function(Elf& elf,const String &func_name,u8* buffer, u32 size);
+void add_function(Elf& elf,const String &func_name,u8* buffer, u32 size)
+{
+    auto& text = elf.text_section;
+
+    // insert the shellcode
+    const u32 offset = push_mem(text.buffer,buffer,size);
+
+    // record the offset
+    add(text.offset_table,func_name,offset);
+}
 
 void write_elf(Elf& elf)
 {
@@ -108,8 +156,7 @@ void write_elf(Elf& elf)
         crash_and_burn("could not open file %s\n",elf.name.buf);
     }
 
-    // for now just dump the header out
-    file_write_arr(fp,(u8*)&elf.header,sizeof(elf.header));
+    file_write_arr(fp,elf.buffer.data,elf.buffer.size);
 
     // TODO: set the executable perms on the file
 
@@ -130,6 +177,8 @@ void build_test_binary()
     */
     // add_function(elf,"_start",SHELLCODE,sizeof(SHELLCODE));
     
+    finalise_elf(elf);
+
     write_elf(elf);
     destroy_elf(elf);
 }
