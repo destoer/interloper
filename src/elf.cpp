8861603754c2aa9f07e6b10600ee2195b24de764
destoer <destoer1234@protonmail.com>
2025-09-07 15:21:50 +0100

    Fix rel link offset

--- src/elf.cpp
+++ src/elf.cpp
@@ -671,7 +671,7 @@ void rewrite_rel_load_store(Interloper& itl,Elf& elf,const LinkOpcode& link)
 
     const auto opcode = link.opcode;
 
-    const s64 section_offset = opcode.v[2].raw;
+    const s64 section_offset = opcode.offset;
     const auto spec = spec_reg(opcode.v[1].raw);
 
     const u32 text_offset = itl.asm_emitter.base_offset;
