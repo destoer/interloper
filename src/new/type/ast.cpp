543dcf4969c39c25c22e0216e9fe040298cd58c9
destoer <destoer1234@protonmail.com>
2025-10-19 19:50:09 +0100

    Start basic type checking on ast

--- src/new/type/ast.cpp
+++ src/new/type/ast.cpp
@@ -1,6 +1,7 @@
 // TODO: Split this fairly immediately.
 
 Option<itl_error> check_startup_func(Interloper& itl, const String& name, NameSpace* name_space);
+Option<itl_error> type_check_expr(Interloper& itl, AstNode* node);
 
 Option<itl_error> check_startup_defs(Interloper& itl)
 {   
@@ -47,6 +48,45 @@ Option<itl_error> check_startup_defs(Interloper& itl)
     return option::none;
 }
 
+Option<itl_error> type_check_decl(Interloper &itl, DeclNode* decl)
+{
+    if(decl->expr)
+    {
+        const auto decl_err = type_check_expr(itl,decl->expr);
+        if(decl_err)
+        {
+            return decl_err;
+        }
+    }
+
+    auto type_res = get_complete_type(itl,decl->type);
+    if(!type_res)
+    {
+        return type_res.error();
+    }
+
+    decl->node.expr_type = *type_res;
+
+    // TODO: check declaration mark symbol slot for decl in node.    
+    // decl->slot = 
+
+
+    return option::none;
+}
+
+Option<itl_error> type_check_expr(Interloper& itl, AstNode* expr)
+{
+    switch(expr->type)
+    {
+        default:
+        {
+            return compile_error(itl,itl_error::invalid_expr,"Type checker(expr) unknown node %s",AST_NAMES[u32(expr->type)]);
+        }
+    }
+
+    return option::none;
+}
+
 // Don't care so much about the legality of structures here, we just want to blindly check the type
 // And do name resolution, as we are going to have to reswitch the AST when we compile anyways.
 
@@ -56,13 +96,19 @@ Option<itl_error> type_check_block(Interloper& itl, AstBlock& block)
     {
         switch(stmt->type)
         {
+            case ast_type::decl:
+            {
+                return type_check_decl(itl, (DeclNode*)stmt);
+            }
+
             default:
             {
-                return compile_error(itl,itl_error::invalid_expr,"Type checker unknown node %s",AST_NAMES[u32(stmt->type)]);
+                return compile_error(itl,itl_error::invalid_expr,"Type checker(stmt) unknown node %s",AST_NAMES[u32(stmt->type)]);
             }
         }
     }
 
+    assert(false);
     return option::none;
 }
 
