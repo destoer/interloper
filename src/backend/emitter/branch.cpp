01834345865b559a802696a8713454c43799b5a9
destoer <destoer1234@protonmail.com>
2026-02-08 12:16:56 +0000

    Add branch reg

--- src/backend/emitter/branch.cpp
+++ src/backend/emitter/branch.cpp
@@ -1,5 +1,13 @@
+ConstRegSpan branch_reg_span(const BranchReg&  branch, RegSpan& span)
+{
+    span.src[0] = branch.src.ir;
+    span.src.size = 1;
+    span.dst.size = 0;
 
-void emit_branch_label(Interloper& itl, Function& func, branch_label_type type, LabelSlot label)
+    return span;
+}
+
+void emit_branch_label(Interloper& itl, Function& func, branch_type type, LabelSlot label)
 {   
     Opcode opcode;
     opcode.group = op_group::branch_label;
@@ -8,7 +16,25 @@ void emit_branch_label(Interloper& itl, Function& func, branch_label_type type, 
     emit_block_func(itl,func,opcode);
 }
 
+void emit_branch_reg(Interloper& itl, Function& func, branch_type type, RegSlot src)
+{   
+    Opcode opcode;
+    opcode.group = op_group::branch_reg;
+    BranchReg branch_reg;
+    branch_reg.type = type;
+    branch_reg.src.ir = src;
+
+    opcode.branch_reg = branch_reg;
+
+    emit_block_func(itl,func,opcode);
+}
+
 void call(Interloper& itl, Function& func, LabelSlot label)
 {
-    emit_branch_label(itl,func,branch_label_type::call,label);
+    emit_branch_label(itl,func,branch_type::call,label);
+}
+
+void call_reg(Interloper& itl, Function& func, RegSlot src)
+{
+    emit_branch_reg(itl,func,branch_type::call,src);
 }
