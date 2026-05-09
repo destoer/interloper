fa13a641dc869fc84621cbf1fca8dd2b21f3337b
destoer <destoer1234@protonmail.com>
2026-05-09 20:41:46 +0100

    Lower unary and mov_imm

--- src/backend/lower_reg.cpp
+++ src/backend/lower_reg.cpp
@@ -1,10 +1,8 @@
-void lower_directive(Opcode& opcode, const ConstLoweredRegSpan& regs)
+void lower_directive(Directive& directive, const ConstLoweredRegSpan& regs)
 {
     u32 src = 0;
     u32 dst = 0;
     u32 dst_src = 0;
-
-    auto& directive = opcode.directive;
 
     for(auto& operand : directive.operand)
     {
@@ -33,6 +31,18 @@ void lower_directive(Opcode& opcode, const ConstLoweredRegSpan& regs)
     }
 }
 
+void lower_mov_gpr_imm(MovGprImm& mov, const ConstLoweredRegSpan& regs)
+{
+    mov.dst.reg = regs.dst[0];
+}
+
+template<typename op_type, op_group group>
+void lower_unary_reg2(UnaryReg2<op_type,group>& unary, const ConstLoweredRegSpan& regs)
+{
+    unary.dst.reg = regs.dst[0];
+    unary.src.reg = regs.src[0];
+}
+
 void lower_opcode(Opcode& opcode, const ConstLoweredRegSpan& regs)
 {
     UNUSED(regs);
@@ -41,9 +51,24 @@ void lower_opcode(Opcode& opcode, const ConstLoweredRegSpan& regs)
     {
         case op_group::directive:
         {
-            lower_directive(opcode,regs);
+            lower_directive(opcode.directive,regs);
             break;
         }
+
+        case op_group::mov_gpr_imm:
+        {
+            lower_mov_gpr_imm(opcode.mov_gpr_imm,regs);
+            break;
+        }
+
+        case op_group::unary_reg2:
+        {
+            lower_unary_reg2(opcode.unary_reg2,regs);
+            break;
+        }
+
+        case op_group::implicit: break;
+        case op_group::branch_label: break;
 
         default:
         {
